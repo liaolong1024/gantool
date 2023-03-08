d963a85ddbc0c5756d50f4d439e0cfa856d2df57
coderliaolong <coderliaolong@outlook.com>
2023-03-08 16:17:08 +0800

    init

---
+++ src/main/java/com/liaolong1024/gantool/GantoolApplication.java
@@ -0,0 +1,13 @@
+package com.liaolong1024.gantool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GantoolApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GantoolApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/liaolong1024/gantool/GantoolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.liaolong1024.gantool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GantoolApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
